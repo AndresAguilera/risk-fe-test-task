83c133a583ba0293e4a87ba0e1d63425b64f5cdd
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-17 18:43:57 -0400

    added mock data and addresses for some currencies

--- components/PairSelector/index.tsx
+++ components/PairSelector/index.tsx
@@ -1,14 +1,8 @@
 import React, { FC, useState } from "react";
 import { Carat } from "@/components/Icons/Icons";
-import { Token } from "@/model/token";
+import { tokens } from "@/data/mock";
 
 interface PairSelectorProps {}
-
-const tokens: Token[] = [
-  { id: "1", name: "BTC" },
-  { id: "2", name: "ETH" },
-  { id: "3", name: "USD" },
-];
 
 const PairSelector: FC<PairSelectorProps> = () => {
   const [token1, setToken1] = useState("");
@@ -25,9 +19,8 @@ const PairSelector: FC<PairSelectorProps> = () => {
               onChange={(e) => setToken1(e.target.value)}
               className="block appearance-none w-full py-2 px-3 pr-8 bg-white border border-gray-300 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
             >
-              <option value="">Select Token 1</option>
               {tokens.map((token) => (
-                <option key={token.id} value={token.name}>
+                <option key={token.address} value={token.address}>
                   {token.name}
                 </option>
               ))}
@@ -42,9 +35,8 @@ const PairSelector: FC<PairSelectorProps> = () => {
               onChange={(e) => setToken2(e.target.value)}
               className="block appearance-none w-full py-2 px-3 pr-8 bg-white border border-gray-300 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
             >
-              <option value="">Select Token 2</option>
               {tokens.map((token) => (
-                <option key={token.id} value={token.name}>
+                <option key={token.address} value={token.address}>
                   {token.name}
                 </option>
               ))}
---
+++ data/mock.ts
@@ -0,0 +1,74 @@
+import { Token } from "@/model/token";
+
+export const tokens: Token[] = [
+  {
+    address: "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+    name: "Uniswap",
+    code: "UNI",
+  },
+  {
+    address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    name: "Ethereum",
+    code: "ETH",
+  },
+  {
+    address: "0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9",
+    name: "Aave",
+    code: "AAVE",
+  },
+  {
+    address: "0x514910771af9ca656af840dff83e8264ecf986ca",
+    name: "Chainlink",
+    code: "LINK",
+  },
+  {
+    address: "0x111111111117dc0aa78b770fa6a738034120c302",
+    name: "1inch",
+    code: "1INCH",
+  },
+  {
+    address: "0x55d398326f99059ff775485246999027b3197955",
+    name: "Binance Coin",
+    code: "BNB",
+  },
+  {
+    address: "0x3d3d35bb9bec23b06ca00fe472b50e7a4c692c30",
+    name: "SushiSwap",
+    code: "SUSHI",
+  },
+  {
+    address: "0xba11d00c5f74255f56a5e366f4f77f5a186d7f55",
+    name: "Band Protocol",
+    code: "BAND",
+  },
+  {
+    address: "0x6b175474e89094c44da98b954eedeac495271d0f",
+    name: "Dai",
+    code: "DAI",
+  },
+  {
+    address: "0x7ab6eedd9443e3e5093ac9c2b271db1295b6c55c",
+    name: "Axie Infinity",
+    code: "AXS",
+  },
+  {
+    address: "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2",
+    name: "Maker",
+    code: "MKR",
+  },
+  {
+    address: "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+    name: "Wrapped Bitcoin",
+    code: "WBTC",
+  },
+  {
+    address: "0x0d8775f648430679a709e98d2b0cb6250d2887ef",
+    name: "Basic Attention Token",
+    code: "BAT",
+  },
+  {
+    address: "0x5dbcf33d8c2e976c6b560249878e6f1491bca25c",
+    name: "The Sandbox",
+    code: "SAND",
+  },
+];
