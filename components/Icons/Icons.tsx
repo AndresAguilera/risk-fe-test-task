411fe1b672f8f72075ea74a3f544505a040cedfc
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-17 11:41:33 -0400

    added basic PairSelector component

---
+++ components/Icons/Icons.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Carat = () => <svg
+    className="fill-current h-4 w-4"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20"
+>
+    <path
+        fillRule="evenodd"
+        d="M14.293 7.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L11.414 10l2.879-2.879z"
+        clipRule="evenodd"
+    />
+</svg>
