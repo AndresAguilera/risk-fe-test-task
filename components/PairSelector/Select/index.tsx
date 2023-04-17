dc352d664b9ce57cf0de030bd13f501980f0d7f8
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-17 18:56:46 -0400

    extract select component

---
+++ components/PairSelector/Select/index.tsx
@@ -0,0 +1,32 @@
+import React, { EventHandler, FC } from "react";
+import { tokens } from "@/data/mock";
+import { Token } from "@/model/token";
+import { Carat } from "@/components/Icons/Icons";
+
+interface SelectProps {
+  token: Token;
+  onChange: EventHandler<any>;
+}
+
+const Select: FC<SelectProps> = ({ token, onChange }) => {
+  return (
+    <div className="relative">
+      <select
+        value={token.address}
+        onChange={onChange}
+        className="block appearance-none w-full py-2 px-3 pr-8 bg-white border border-gray-300 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
+      >
+        {tokens.map((token) => (
+          <option key={token.address} value={token.address}>
+            {token.name}
+          </option>
+        ))}
+      </select>
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+        <Carat />
+      </div>
+    </div>
+  );
+};
+
+export default Select;
--- components/PairSelector/index.tsx
+++ components/PairSelector/index.tsx
@@ -1,50 +1,21 @@
 import React, { FC, useState } from "react";
-import { Carat } from "@/components/Icons/Icons";
 import { tokens } from "@/data/mock";
+import Select from "@/components/PairSelector/Select";
+import { Token } from "@/model/token";
 
 interface PairSelectorProps {}
 
 const PairSelector: FC<PairSelectorProps> = () => {
-  const [token1, setToken1] = useState("");
-  const [token2, setToken2] = useState("");
+  const [token1, setToken1] = useState<Token>(tokens[0]);
+  const [token2, setToken2] = useState<Token>(tokens[1]);
 
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col space-y-8 ">
         <h1 className="text-2xl font-bold">Select a trading pair</h1>
         <div className="flex space-x-4">
-          <div className="relative">
-            <select
-              value={token1}
-              onChange={(e) => setToken1(e.target.value)}
-              className="block appearance-none w-full py-2 px-3 pr-8 bg-white border border-gray-300 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-            >
-              {tokens.map((token) => (
-                <option key={token.address} value={token.address}>
-                  {token.name}
-                </option>
-              ))}
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <Carat />
-            </div>
-          </div>
-          <div className="relative">
-            <select
-              value={token2}
-              onChange={(e) => setToken2(e.target.value)}
-              className="block appearance-none w-full py-2 px-3 pr-8 bg-white border border-gray-300 rounded-md shadow-sm leading-tight focus:outline-none focus:shadow-outline-blue focus:border-blue-300"
-            >
-              {tokens.map((token) => (
-                <option key={token.address} value={token.address}>
-                  {token.name}
-                </option>
-              ))}
-            </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-              <Carat />
-            </div>
-          </div>
+          <Select token={token1} onChange={(e) => setToken1(e.target.value)} />
+          <Select token={token2} onChange={(e) => setToken2(e.target.value)} />
         </div>
       </div>
     </div>
