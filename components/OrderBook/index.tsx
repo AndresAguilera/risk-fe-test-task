749f1acdd68295386abea5ba01ff44d2fe0f3be4
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-19 12:26:50 -0400

    useOrderBook hook now holds order book logic

--- components/OrderBook/index.tsx
+++ components/OrderBook/index.tsx
@@ -1,75 +1,14 @@
 import React, { useContext } from 'react'
-import { Order } from '@/model/order'
 import { useOrderBook } from '@/hooks/useOrderBook'
 import { PairContext } from '@/pages'
-import { considerDecimals, getCodeByAddress, getTokenByAddress } from '@/utils'
-
-interface OrderBookRow extends Order {
-    price: number
-    quantity: number
-    total: number
-}
+import { getCodeByAddress } from '@/utils'
 
 const OrderBook: React.FC = () => {
     const { baseToken, quoteToken } = useContext(PairContext)
-    const { data } = useOrderBook({ baseToken, quoteToken })
+    const { asks, bids } = useOrderBook({ baseToken, quoteToken })
     // useSubscription({ makerToken: baseToken, takerToken: quoteToken })
 
     const currentCurrency = getCodeByAddress(quoteToken)
-
-    const bids: OrderBookRow[] =
-        data?.bids?.records?.map((record: any) => {
-            const order: Order = record.order
-            const maker = getTokenByAddress(order.makerToken)
-            const taker = getTokenByAddress(order.takerToken)
-            const quantity = considerDecimals(order.makerAmount, maker?.decimals)
-            const takerAmount = considerDecimals(order.takerAmount, taker?.decimals)
-            const makerAmount = considerDecimals(order.makerAmount, maker?.decimals)
-            const price = makerAmount / takerAmount
-
-            return {
-                ...order,
-                price,
-                quantity,
-                total: 0,
-            }
-        }) || []
-
-    const asks: OrderBookRow[] =
-        data?.asks?.records?.map((record: any) => {
-            const order: Order = record.order
-
-            const maker = getTokenByAddress(order.makerToken)
-            const taker = getTokenByAddress(order.takerToken)
-
-            const makerDecimals = maker?.decimals
-            const takerDecimals = taker?.decimals
-
-            const quantity = considerDecimals(order.takerAmount, takerDecimals)
-
-            const takerAmount = considerDecimals(order.takerAmount, takerDecimals)
-            const makerAmount = considerDecimals(order.makerAmount, makerDecimals)
-            const price = takerAmount / makerAmount
-
-            return {
-                ...order,
-                price,
-                quantity,
-                total: 0,
-            }
-        }) || []
-
-    const addTotal = (rows: OrderBookRow[]) =>
-        rows.forEach((row, i) => {
-            if (i === 0) {
-                row.total = row.quantity
-            } else {
-                row.total = row.quantity + rows[i - 1].total
-            }
-        })
-
-    addTotal(bids)
-    addTotal(asks)
 
     return (
         <div className="flex justify-between">
--- hooks/useOrderBook.tsx
+++ hooks/useOrderBook.tsx
@@ -1,8 +1,71 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchOrderBook, FetchOrderBookArgs } from "@/data/api/0x";
+import { useQuery } from '@tanstack/react-query'
+import { fetchOrderBook, FetchOrderBookArgs } from '@/data/api/0x'
+import { Order } from '@/model/order'
+import { considerDecimals, getTokenByAddress } from '@/utils'
 
-export const useOrderBook = (data: FetchOrderBookArgs) =>
-  useQuery({
-    queryKey: ["order book", data.baseToken, data.quoteToken],
-    queryFn: () => fetchOrderBook(data),
-  });
+export interface OrderBookRow extends Order {
+    price: number
+    quantity: number
+    total: number
+}
+
+const addTotal = (rows: OrderBookRow[]) =>
+    rows.forEach((row, i) => {
+        if (i === 0) {
+            row.total = row.quantity
+        } else {
+            row.total = row.quantity + rows[i - 1].total
+        }
+    })
+export const useOrderBook = ({ baseToken, quoteToken }: FetchOrderBookArgs) => {
+    const query = useQuery({
+        queryKey: ['order book', baseToken, quoteToken],
+        queryFn: () => fetchOrderBook({ baseToken, quoteToken }),
+    })
+    const bids: OrderBookRow[] =
+        query.data?.bids?.records?.map((record: any) => {
+            const order: Order = record.order
+            const maker = getTokenByAddress(order.makerToken)
+            const taker = getTokenByAddress(order.takerToken)
+            const quantity = considerDecimals(order.makerAmount, maker?.decimals)
+            const takerAmount = considerDecimals(order.takerAmount, taker?.decimals)
+            const makerAmount = considerDecimals(order.makerAmount, maker?.decimals)
+            const price = makerAmount / takerAmount
+
+            return {
+                ...order,
+                price,
+                quantity,
+                total: 0,
+            }
+        }) || []
+
+    const asks: OrderBookRow[] =
+        query.data?.asks?.records?.map((record: any) => {
+            const order: Order = record.order
+
+            const maker = getTokenByAddress(order.makerToken)
+            const taker = getTokenByAddress(order.takerToken)
+
+            const makerDecimals = maker?.decimals
+            const takerDecimals = taker?.decimals
+
+            const quantity = considerDecimals(order.takerAmount, takerDecimals)
+
+            const takerAmount = considerDecimals(order.takerAmount, takerDecimals)
+            const makerAmount = considerDecimals(order.makerAmount, makerDecimals)
+            const price = takerAmount / makerAmount
+
+            return {
+                ...order,
+                price,
+                quantity,
+                total: 0,
+            }
+        }) || []
+
+    addTotal(bids)
+    addTotal(asks)
+
+    return { ...query, asks, bids }
+}
