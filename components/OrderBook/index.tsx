86faae3cc9403efa3148b9061506beee6fee3987
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-18 21:34:28 -0400

    connect to websocket, logging response and orders

--- components/OrderBook/index.tsx
+++ components/OrderBook/index.tsx
@@ -1,129 +1,123 @@
-import React, { useContext } from "react";
-import { Order } from "@/model/order";
-import { useOrderBook } from "@/hooks/useOrderBook";
-import { PairContext } from "@/pages";
-import { formatCurrency, getCodeByAddress, getTokenByAddress } from "@/utils";
-import useSubscription from "@/hooks/useSubscription";
+import React, { useContext } from 'react'
+import { Order } from '@/model/order'
+import { useOrderBook } from '@/hooks/useOrderBook'
+import { PairContext } from '@/pages'
+import { formatCurrency, getCodeByAddress, getTokenByAddress } from '@/utils'
+import useSubscription from '@/hooks/useSubscription'
 
 interface OrderBookRow extends Order {
-  price: number;
-  quantity: number;
-  total: number;
+    price: number
+    quantity: number
+    total: number
 }
 
 const OrderBook: React.FC = () => {
-  const { baseToken, quoteToken } = useContext(PairContext);
-  const { data } = useOrderBook({ baseToken, quoteToken });
-  useSubscription({ makerToken: baseToken, takerToken: quoteToken });
+    const { baseToken, quoteToken } = useContext(PairContext)
+    const { data } = useOrderBook({ baseToken, quoteToken })
+    useSubscription({ makerToken: baseToken, takerToken: quoteToken })
 
-  const currentCurrency = getCodeByAddress(quoteToken);
+    const currentCurrency = getCodeByAddress(quoteToken)
 
-  const bids: OrderBookRow[] =
-    data?.bids?.records
-      ?.map((record: any) => {
-        const order: Order = record.order;
-        const decimals = getTokenByAddress(order.makerToken)?.decimals;
-        const quantity = formatCurrency(order.makerAmount, decimals);
-        const price = formatCurrency(
-          order.takerAmount / order.makerAmount,
-          decimals
-        );
+    const bids: OrderBookRow[] =
+        data?.bids?.records
+            ?.map((record: any) => {
+                const order: Order = record.order
+                const decimals = getTokenByAddress(order.makerToken)?.decimals
+                const quantity = formatCurrency(order.makerAmount, decimals)
+                const price = formatCurrency(order.takerAmount / order.makerAmount, decimals)
 
-        const total = 0;
+                const total = 0
 
-        return {
-          ...order,
-          price,
-          quantity,
-          total,
-        };
-      })
-      .reverse() || [];
+                return {
+                    ...order,
+                    price,
+                    quantity,
+                    total,
+                }
+            })
+            .reverse() || []
 
-  bids.forEach((bid, i) => {
-    if (i === 0) {
-      bid.total = bid.quantity;
-    } else {
-      bid.total = bid.quantity + bids[i - 1].total;
-    }
-  });
+    bids.forEach((bid, i) => {
+        if (i === 0) {
+            bid.total = bid.quantity
+        } else {
+            bid.total = bid.quantity + bids[i - 1].total
+        }
+    })
 
-  const asks: OrderBookRow[] =
-    data?.asks?.records
-      ?.map((record: any) => {
-        const order: Order = record.order;
-        const decimals = getTokenByAddress(order.takerToken)?.decimals;
-        const quantity = formatCurrency(order.takerAmount, decimals);
-        const price = formatCurrency(
-          order.makerAmount / order.takerAmount,
-          decimals
-        );
+    const asks: OrderBookRow[] =
+        data?.asks?.records
+            ?.map((record: any) => {
+                const order: Order = record.order
+                const decimals = getTokenByAddress(order.takerToken)?.decimals
+                const quantity = formatCurrency(order.takerAmount, decimals)
+                const price = formatCurrency(order.makerAmount / order.takerAmount, decimals)
 
-        const total = 0;
+                const total = 0
 
-        return {
-          ...order,
-          price,
-          quantity,
-          total,
-        };
-      })
-      .reverse() || [];
+                return {
+                    ...order,
+                    price,
+                    quantity,
+                    total,
+                }
+            })
+            .reverse() || []
 
-  asks.forEach((ask, i) => {
-    if (i === 0) {
-      ask.total = ask.quantity;
-    } else {
-      ask.total = ask.quantity + asks[i - 1].total;
-    }
-  });
+    asks.forEach((ask, i) => {
+        if (i === 0) {
+            ask.total = ask.quantity
+        } else {
+            ask.total = ask.quantity + asks[i - 1].total
+        }
+    })
 
-  return (
-    <div className="flex justify-between">
-      <div className="flex-1 pr-4">
-        <h2 className="text-2xl font-semibold mb-2">Bids</h2>
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th className="text-left">Price ({currentCurrency})</th>
-              <th className="text-left">Quantity ({currentCurrency})</th>
-              <th className="text-left">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {bids?.map((bid, i) => (
-              <tr key={bid.chainId + bid.takerToken + i}>
-                <td className="text-green-500">{bid.price}</td>
-                <td>{bid.quantity}</td>
-                <td>{bid.total}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="flex-1 pl-4">
-        <h2 className="text-2xl font-semibold mb-2">Asks</h2>
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th className="text-left">Price ({currentCurrency})</th>
-              <th className="text-left">Quantity ({currentCurrency})</th>
-              <th className="text-left">Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {asks?.map((ask, i) => (
-              <tr key={ask.chainId + ask.makerToken + i}>
-                <td className="text-red-500">{ask.price}</td>
-                <td>{ask.quantity}</td>
-                <td>{ask.total}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="flex justify-between">
+            <div className="flex-1 pr-4">
+                <h2 className="text-2xl font-semibold mb-2">Bids</h2>
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th className="text-left">Price ({currentCurrency})</th>
+                            <th className="text-left">Quantity ({currentCurrency})</th>
+                            <th className="text-left">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {bids?.map((bid, i) => (
+                            <tr key={bid.chainId + bid.takerToken + i}>
+                                <td className="text-green-500">{bid.price}</td>
+                                <td>{bid.quantity}</td>
+                                <td>{bid.total}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="flex-1 pl-4">
+                <h2 className="text-2xl font-semibold mb-2">Asks</h2>
+                <table className="w-full">
+                    <thead>
+                        <tr>
+                            <th className="text-left">Price ({currentCurrency})</th>
+                            <th className="text-left">Quantity ({currentCurrency})</th>
+                            <th className="text-left">Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {asks?.map((ask, i) => (
+                            <tr key={ask.chainId + ask.makerToken + i}>
+                                <td className="text-red-500">{ask.price}</td>
+                                <td>{ask.quantity}</td>
+                                <td>{ask.total}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
 
-export default OrderBook;
+export default OrderBook
--- hooks/useSubscription.tsx
+++ hooks/useSubscription.tsx
@@ -1,40 +1,66 @@
-import React, { useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { useEffect, useState } from 'react'
 import { useQueryClient } from '@tanstack/react-query'
+import { Order } from '@/model/order'
 
 interface useSubscriptionProps {
     makerToken?: string
     takerToken?: string
 }
 
+interface SubscriptionMessage {
+    type: 'subscribe'
+    channel: 'orders'
+    requestId?: string
+    payload?: {
+        makerToken: string
+        takerToken: string
+    }
+}
+
+interface ResponseMessage {
+    type: string
+    channel: string
+    payload: { order: Order; metadata: object }[]
+    requestId: string
+}
+
 const useSubscription = ({ makerToken = '', takerToken = '' }: useSubscriptionProps) => {
     const queryClient = useQueryClient()
+    const [orders, setOrders] = useState<Order[]>([])
 
     useEffect(() => {
         const websocket = new WebSocket(
-            `wss://api.0x.org/orderbook/v1?makerToken=${makerToken}&takerToken=${takerToken}/`
+            `wss://api.0x.org/orderbook/v1?makerToken=${makerToken}&takerToken=${takerToken}`
         )
-        websocket.onopen = (ev) => {
-            console.log('connected', ev)
+        websocket.onopen = () => {
+            console.log('Connected to WebSocket')
+            const subscriptionMessage: SubscriptionMessage = {
+                type: 'subscribe',
+                channel: 'orders',
+                requestId: uuidv4(),
+            }
+            websocket.send(JSON.stringify(subscriptionMessage))
         }
-        websocket.onclose = () => {
-            console.log('closed')
+        websocket.onclose = (ev) => {
+            console.log('WebSocket connection closed', ev.code)
+        }
+        websocket.onerror = (error) => {
+            console.error('WebSocket error:', error)
         }
         websocket.onmessage = (event) => {
-            console.log('msg', event)
-            const data = JSON.parse(event.data)
-            queryClient.setQueriesData(data, (oldData) => {
-                console.log(data, oldData)
-                return data
-                // const update = (entity: any) =>
-                //     entity.id === data.id ? { ...entity, ...data.payload } : entity
-                // return Array.isArray(oldData) ? oldData.map(update) : update(oldData)
-            })
+            const data: ResponseMessage = JSON.parse(event.data)
+            const order: Order = data.payload[0].order
+            setOrders([...orders, order])
         }
-
         return () => {
             websocket.close()
         }
-    }, [queryClient])
+    }, [makerToken, takerToken, queryClient])
+
+    useEffect(() => {
+        console.log({ orders })
+    }, [orders])
 }
 
 export default useSubscription
