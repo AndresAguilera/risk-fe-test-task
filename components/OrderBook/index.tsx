a2cc9a259daa86bb153800cc0a665dd3beba6c27
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-21 16:38:34 -0400

    fix tailwind issue with order book

--- components/OrderBook/index.tsx
+++ components/OrderBook/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { OrderBookRow, useOrderBook } from '@/hooks/useOrderBook'
+import { useOrderBook } from '@/hooks/useOrderBook'
 import { formatCurrency, getTokenByAddress } from '@/utils'
 import { PairContext } from '@/context/tokenPair'
 
@@ -8,21 +8,16 @@ const OrderBook: React.FC = () => {
     const { asks, bids } = useOrderBook({ baseToken, quoteToken })
 
     const currentCurrency = getTokenByAddress(quoteToken)?.symbol
+    const headers = ['Price', 'Quantity', 'Total']
 
-    const renderOrders = (orders: OrderBookRow[], isBid: boolean) => {
-        const gradient = isBid
-            ? 'bg-gradient-to-r from-green-500 to-blue-500'
-            : 'bg-gradient-to-l from-pink-500 to-red-500'
-
-        const greenOrRed = isBid ? 'green' : 'red'
-        const headers = ['Price', 'Quantity', 'Total']
-        return (
+    return (
+        <div className="flex flex-col sm:flex-row justify-between">
             <div className="flex-1 mr-4 block">
                 <div className="bg-white rounded-lg shadow-md overflow-hidden text-sm">
-                    <div className={`flex ${gradient} text-white font-bold`}>
-                        {(isBid ? headers : headers.reverse()).map((header, i) => (
+                    <div className="flex bg-gradient-to-r from-green-500 to-blue-500 text-white font-bold">
+                        {headers.reverse().map((header, i) => (
                             <div
-                                key={header + isBid}
+                                key={header + false}
                                 className={`flex-1 py-3 px-4 tracking-wider whitespace-nowrap ${
                                     i === 1 ? 'hidden lg:block' : ''
                                 }`}
@@ -31,8 +26,8 @@ const OrderBook: React.FC = () => {
                             </div>
                         ))}
                     </div>
-                    {orders?.slice(0, 20).map((order, i, array) => {
-                        const maxTotal = orders[array.length - 1].total
+                    {bids?.slice(0, 20).map((order, i, array) => {
+                        const maxTotal = bids[array.length - 1].total
                         const curTotal = order.total
                         const arbMax = 13
 
@@ -41,7 +36,7 @@ const OrderBook: React.FC = () => {
                         const row = [
                             <div
                                 key={order.price + 'price'}
-                                className={`flex-1 text-${greenOrRed}-500 font-medium py-3 pl-2`}
+                                className="flex-1 text-green-500 font-medium py-3 pl-2"
                             >
                                 {formatCurrency(order.price)}
                             </div>,
@@ -57,26 +52,73 @@ const OrderBook: React.FC = () => {
                             >
                                 <div
                                     style={{ width: `${barWidth}%` }}
-                                    className={`absolute h-5 bg-${greenOrRed}-200 z-0`}
+                                    className="absolute h-5 bg-green-200 z-0"
                                 />
-                                <div className={'z-10 relative'}>{formatCurrency(order.total)}</div>
+                                <div className="z-10 relative">{formatCurrency(order.total)}</div>
                             </div>,
                         ]
                         return (
-                            <div key={order.salt + i + 'row'} className={`flex hover:bg-gray-200`}>
-                                {isBid ? row : row.reverse()}
+                            <div key={order.salt + i + 'row'} className="flex hover:bg-gray-200">
+                                {row}
                             </div>
                         )
                     })}
                 </div>
             </div>
-        )
-    }
 
-    return (
-        <div className="flex flex-col sm:flex-row justify-between">
-            {renderOrders(bids, true)}
-            {renderOrders(asks, false)}
+            <div className="flex-1 mr-4 block">
+                <div className="bg-white rounded-lg shadow-md overflow-hidden text-sm">
+                    <div className="flex bg-gradient-to-r from-pink-500 to-red-500 text-white font-bold">
+                        {headers.reverse().map((header, i) => (
+                            <div
+                                key={header + false}
+                                className={`flex-1 py-3 px-4 tracking-wider whitespace-nowrap ${
+                                    i === 1 ? 'hidden lg:block' : ''
+                                }`}
+                            >
+                                {header} ({currentCurrency})
+                            </div>
+                        ))}
+                    </div>
+                    {asks?.slice(0, 20).map((order, i, array) => {
+                        const maxTotal = asks[array.length - 1].total
+                        const curTotal = order.total
+                        const arbMax = 13
+
+                        const ratio = Math.round((curTotal / maxTotal) * arbMax)
+                        const barWidth = ratio > arbMax ? arbMax : ratio === 0 ? 1 : ratio
+                        const row = [
+                            <div
+                                key={order.price + 'price'}
+                                className="flex-1 text-red-500 font-medium py-3 pl-2"
+                            >
+                                {formatCurrency(order.price)}
+                            </div>,
+                            <div
+                                key={order.quantity + 'qty'}
+                                className="flex-1 py-3 hidden lg:block"
+                            >
+                                {formatCurrency(order.quantity)}
+                            </div>,
+                            <div
+                                key={order.total + order.salt + i + 'total'}
+                                className="flex-1 py-3"
+                            >
+                                <div
+                                    style={{ width: `${barWidth}%` }}
+                                    className="absolute h-5 bg-red-200 z-0"
+                                />
+                                <div className="z-10 relative">{formatCurrency(order.total)}</div>
+                            </div>,
+                        ]
+                        return (
+                            <div key={order.salt + i + 'row'} className="flex hover:bg-gray-200">
+                                {row.reverse()}
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </div>
     )
 }
