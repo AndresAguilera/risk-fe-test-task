96212a295ac8e45a8cc50aa837f73076228f22ad
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-21 15:14:59 -0400

    updated orderbook UI

--- components/OrderBook/index.tsx
+++ components/OrderBook/index.tsx
@@ -14,34 +14,46 @@ const OrderBook: React.FC = () => {
             ? 'bg-gradient-to-r from-green-500 to-blue-500'
             : 'bg-gradient-to-l from-pink-500 to-red-500'
 
+        const greenOrRed = isBid ? 'green' : 'red'
+        const headers = ['Price', 'Quantity', 'Total']
         return (
             <div className="flex-1 mr-4 block">
                 <h2 className="text-2xl font-semibold mb-2">{isBid ? 'Bids' : 'Asks'}</h2>
                 <div className="bg-white rounded-lg shadow-md overflow-hidden text-sm">
-                    <div className={`flex ${gradient} text-white`}>
-                        <div className="flex-1 py-3 px-4 tracking-wider whitespace-nowrap">
-                            Price ({currentCurrency})
-                        </div>
-                        <div className="flex-1 py-3 px-4 tracking-wider whitespace-nowrap hidden lg:block">
-                            Quantity ({currentCurrency})
-                        </div>
-                        <div className="flex-1 py-3 px-4 tracking-wider whitespace-nowrap">
-                            Total
-                        </div>
-                    </div>
-                    {orders?.slice(0, 20).map((order, i) => (
-                        <div key={order.salt + i} className={`flex hover:bg-gray-200`}>
+                    <div className={`flex ${gradient} text-white font-bold`}>
+                        {(isBid ? headers : headers.reverse()).map((header, i) => (
                             <div
-                                className={`flex-1 text-${
-                                    isBid ? 'green' : 'red'
-                                }-500 font-medium py-3 pl-2`}
+                                key={header}
+                                className={`flex-1 py-3 px-4 tracking-wider whitespace-nowrap ${
+                                    i === 1 ? 'hidden lg:block' : ''
+                                }`}
+                            >
+                                {header} ({currentCurrency})
+                            </div>
+                        ))}
+                    </div>
+                    {orders?.slice(0, 20).map((order, i) => {
+                        const row = [
+                            <div
+                                key={order.price}
+                                className={`flex-1 text-${greenOrRed}-500 font-medium py-3 pl-2`}
                             >
                                 {order.price}
+                            </div>,
+                            <div key={order.quantity} className="flex-1 py-3 hidden lg:block">
+                                {order.quantity}
+                            </div>,
+                            <div key={order.total} className="flex-1 py-3">
+                                <div className={`absolute w-[13%] h-5 bg-${greenOrRed}-200 z-0`} />
+                                <div className={'z-10 relative'}>{order.total}</div>
+                            </div>,
+                        ]
+                        return (
+                            <div key={order.salt + i} className={`flex hover:bg-gray-200`}>
+                                {isBid ? row : row.reverse()}
                             </div>
-                            <div className="flex-1 py-3 px-4 hidden lg:block">{order.quantity}</div>
-                            <div className="flex-1 py-3 px-4">{order.total}</div>
-                        </div>
-                    ))}
+                        )
+                    })}
                 </div>
             </div>
         )
