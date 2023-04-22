e1746ecb1a1cb1a280ad6b265d47b092b9092e4a
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-22 19:55:17 -0400

    updated text

--- components/LiveOrders/index.tsx
+++ components/LiveOrders/index.tsx
@@ -33,22 +33,26 @@ const LiveOrders = () => {
         },
     ]
     return (
-        <div>
-            <div className={'font-extralight text-sm italic text-gray-500 mb-2'}>
+        <div className={'rounded-2xl border border-gray-400 p-4'}>
+            <h1 className={'font-bold'}>Live Orders</h1>
+
+            <div className={'font-extralight text-sm italic text-gray-500 mb-2 my-2'}>
                 <p>
                     *The amount of messages received for a specific token pair from the web socket
-                    is very low, so you may have to sit there a while to get a visual update. For
-                    that, I have placed the Live Orders component below to display the orders
-                    passed.
+                    is very low, so you may have to sit there a while to get a visual update for the
+                    Order Book UI below. To alleviate that, I have placed this Live Orders component
+                    display ALL the orders added live.
                 </p>
-                <p>*If the toggle is off, the symbol is only displayed if known (faster).</p>
+                <p>
+                    *If the toggle is off, the symbol is only displayed if known; otherwise, it
+                    displays the address. (faster).
+                </p>
                 <p>*If the toggle is on, it will only display known token symbols (slower)</p>
             </div>
-            <div className={'mb-2'}>
+            <div className={'my-2'}>
                 <span className={'mr-4 italic'}>Show Only Known Tokens</span>
                 <Toggle isOn={onlyKnownTokens} handleToggle={handleToggle} />
             </div>
-            <h1 className={'font-bold'}>Live Orders</h1>
             {orderData.map(({ label, value, sym }, i) => (
                 <span key={`label${i}`} className={'mr-4'}>
                     <span className={'font-bold'}>{label}</span>
