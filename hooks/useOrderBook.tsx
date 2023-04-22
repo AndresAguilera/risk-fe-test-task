437bd2f36bc070bfe346740fee02781a855cc92d
Andres Aguilera <andres.aguilera.mendez@gmail.com>
2023-04-22 16:58:13 -0400

    useSubscription hook now returns message data as orders

--- hooks/useOrderBook.tsx
+++ hooks/useOrderBook.tsx
@@ -1,8 +1,8 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { fetchOrderBook, FetchOrderBookArgs } from '@/data/api/0x'
 import { Order } from '@/model/order'
 import { considerDecimals, getTokenByAddress } from '@/utils'
-import { useMemo } from 'react'
+import { useEffect, useMemo } from 'react'
 import useSubscription from '@/hooks/useSubscription'
 
 export interface OrderBookRow extends Order {
@@ -21,41 +21,65 @@ const addTotal = (rows: OrderBookRow[]) =>
     })
 export const processRecords = (
     records: { order: Order; metadata?: any }[],
-    isBid: boolean,
-    ws: boolean = false // TODO: used for testing for now, DELETE ME
-): OrderBookRow[] =>
-    records
-        ?.map((record: any) => {
-            const order: Order = record.order
-            const maker = getTokenByAddress(order.makerToken)
-            const taker = getTokenByAddress(order.takerToken)
-            const quantity = considerDecimals(
-                isBid ? order.makerAmount : order.takerAmount,
-                isBid ? maker?.decimals : taker?.decimals
-            )
-            const takerAmount = considerDecimals(order.takerAmount, taker?.decimals)
-            const makerAmount = considerDecimals(order.makerAmount, maker?.decimals)
-            const price = isBid ? makerAmount / takerAmount : takerAmount / makerAmount
-            if (ws) {
-                // console.log(order.makerToken, order.takerToken)
-            }
-            return {
-                ...order,
-                price,
-                quantity,
-                total: 0,
-            }
-        })
-        .sort((a, b) => (!isBid ? a.price - b.price : b.price - a.price)) || []
+    isBid: boolean
+): OrderBookRow[] => {
+    const result =
+        records
+            ?.map((record: any) => {
+                const order: Order = record.order
+                const maker = getTokenByAddress(order.makerToken)
+                const taker = getTokenByAddress(order.takerToken)
+                const quantity = considerDecimals(
+                    isBid ? order.makerAmount : order.takerAmount,
+                    isBid ? maker?.decimals : taker?.decimals
+                )
+                const takerAmount = considerDecimals(order.takerAmount, taker?.decimals)
+                const makerAmount = considerDecimals(order.makerAmount, maker?.decimals)
+                const price = isBid ? makerAmount / takerAmount : takerAmount / makerAmount
+                return {
+                    ...order,
+                    price,
+                    quantity,
+                    total: 0,
+                }
+            })
+            .sort((a, b) => (!isBid ? a.price - b.price : b.price - a.price)) || []
+    addTotal(result)
+    return result
+}
 
 export const useOrderBook = ({ baseToken, quoteToken }: FetchOrderBookArgs) => {
+    const queryClient = useQueryClient()
+
     const query = useQuery({
         queryKey: ['order book', baseToken, quoteToken],
         queryFn: () => fetchOrderBook({ baseToken, quoteToken }),
         refetchOnWindowFocus: false,
     })
 
-    useSubscription({ makerToken: baseToken, takerToken: quoteToken })
+    const makerToken = baseToken
+    const takerToken = quoteToken
+
+    const orders = useSubscription({ makerToken, takerToken })
+
+    useEffect(() => {
+        if (orders.length) {
+            const rTakerToken = orders[0]?.order.takerToken
+            const rMakerToken = orders[0]?.order.makerToken
+            const isBid = rMakerToken === makerToken && rTakerToken === takerToken
+
+            const askOrBid = isBid ? 'bids' : 'asks'
+
+            const queryKey = ['order book', makerToken, takerToken]
+            queryClient.setQueriesData(queryKey, (oldData: any) => ({
+                ...oldData,
+                [askOrBid]: {
+                    ...oldData[askOrBid],
+                    records: [...oldData[askOrBid].records, ...orders],
+                },
+            }))
+        }
+    }, [orders, makerToken, takerToken, queryClient])
 
     const bids: OrderBookRow[] = useMemo(
         () => processRecords(query.data?.bids?.records, true),
@@ -66,9 +90,6 @@ export const useOrderBook = ({ baseToken, quoteToken }: FetchOrderBookArgs) => {
         () => processRecords(query.data?.asks?.records, false),
         [query]
     )
-
-    addTotal(bids)
-    addTotal(asks)
 
     return { ...query, asks, bids }
 }
--- hooks/useSubscription.tsx
+++ hooks/useSubscription.tsx
@@ -1,6 +1,5 @@
 import { v4 as uuidv4 } from 'uuid'
-import { useEffect } from 'react'
-import { useQueryClient } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
 import { OrderBookRow, processRecords } from '@/hooks/useOrderBook'
 
 interface useSubscriptionProps {
@@ -19,7 +18,7 @@ interface SubscriptionMessage {
 }
 
 const useSubscription = ({ makerToken, takerToken }: useSubscriptionProps) => {
-    const queryClient = useQueryClient()
+    const [orders, setOrders] = useState<{ order: OrderBookRow }[]>([])
 
     useEffect(() => {
         const websocket = new WebSocket(`wss://api.0x.org/orderbook/v1`)
@@ -29,7 +28,10 @@ const useSubscription = ({ makerToken, takerToken }: useSubscriptionProps) => {
                 type: 'subscribe',
                 channel: 'orders',
                 requestId: uuidv4(),
-                payload: { takerToken, makerToken },
+                payload: {
+                    ...(!!takerToken && { takerToken }),
+                    ...(!!makerToken && { makerToken }),
+                },
             }
             websocket.send(JSON.stringify(subscriptionMessage))
         }
@@ -41,32 +43,19 @@ const useSubscription = ({ makerToken, takerToken }: useSubscriptionProps) => {
                 (record: any) => record?.metaData?.state === 'ADDED'
             )
             if (!records.length) return
-            console.log(records[0])
 
             const rTakerToken = records[0]?.order.takerToken
             const rMakerToken = records[0]?.order.makerToken
             const isBid = rMakerToken === makerToken && rTakerToken === takerToken
             // const isBid = !!Math.round(Math.random())
-            const askOrBid = isBid ? 'bids' : 'asks'
 
-            const queryKey = ['order book', makerToken, takerToken]
-            const processedRecords: OrderBookRow[] = processRecords(records, isBid, true)
-
-            queryClient.setQueriesData(queryKey, (oldData: any) => ({
-                ...oldData,
-                [askOrBid]: {
-                    ...oldData[askOrBid],
-                    records: [
-                        ...oldData[askOrBid].records,
-                        ...processedRecords.map((r) => {
-                            return { order: r }
-                        }),
-                    ],
-                },
-            }))
+            const processedRecords: OrderBookRow[] = processRecords(records, isBid)
+            const orders: { order: OrderBookRow }[] = processedRecords.map((r) => ({ order: r }))
+            setOrders(orders)
         }
         return () => websocket.close()
-    }, [makerToken, takerToken, queryClient])
+    }, [makerToken, takerToken])
+    return orders
 }
 
 export default useSubscription
